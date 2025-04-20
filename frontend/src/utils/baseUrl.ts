56e189441cbe6e5dbf1a4692aaa5257bf64ca9f9
Azhar Hayat <azharhayat537271@gmail.com>
2025-04-21 01:57:11 +0500

    deployment configuration

--- frontend/src/utils/baseUrl.ts
+++ frontend/src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api"; 
-export const SHORT_URL = "http://localhost:5000";
+export const BASE_URL = "https://short-tgi9.onrender.com/api"; 
+export const SHORT_URL = "https://short-tgi9.onrender.com";
